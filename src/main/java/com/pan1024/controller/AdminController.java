a7cc7774113e4e8287ca268b52242e9fea9635da
lishuqian <lishuqian@uepay.mo>
2021-02-25 12:20:32 +0800

    CCC

--- src/main/java/com/pan1024/controller/AdminController.java
+++ src/main/java/com/pan1024/controller/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     @Autowired private AdminService adminService;
+//    @Autowired private AdminService adminService;
 
     @GetMapping("/find/all/view")
     public String findAll() {
